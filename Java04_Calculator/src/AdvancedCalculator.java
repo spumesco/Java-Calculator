93232566fc837c27e11cfebc459fee384a3dc0ca
jjhoyun <jjhoyun@gmail.com>
2025-10-02 13:37:14 +0900

    주석 추가

--- Java04_Calculator/src/AdvancedCalculator.java
+++ Java04_Calculator/src/AdvancedCalculator.java
@@ -3,11 +3,15 @@ public class AdvancedCalculator extends Calculator {
     public int multiply(){
         result = num1 * num2;
         return result;
+        // 계산기 2.0 version 곱하기 기능 개발 완료
+        //https://github.com/spumesco/Java-Calculator/issues/2
     }
 
     @Override
     public int divide(){
         result = num1 / num2;
         return result;
+        // 계산기 2.0 version 나누기 기능 개발 완료
+        https://github.com/spumesco/Java-Calculator/issues/3
     }
 }
